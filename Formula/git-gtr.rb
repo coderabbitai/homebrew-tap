d86842d006bc3f214af20e536ff4431b43cfa682
Hans Elizaga <tom.elizaga@gmail.com>
2026-02-01 21:37:42 -0800

    feat: add git-gtr formula

---
+++ Formula/git-gtr.rb
@@ -0,0 +1,25 @@
+class GitGtr < Formula
+  desc "Git worktree manager with editor and AI tool integration"
+  homepage "https://github.com/coderabbitai/git-worktree-runner"
+  url "https://github.com/coderabbitai/git-worktree-runner/archive/refs/tags/v2.1.0.tar.gz"
+  sha256 "7e946e1bccccadcaffd3781da87fa30451165b4016a8d644127c06f612ce7847"
+  license "Apache-2.0"
+
+  depends_on "git"
+
+  def install
+    bin.install "bin/git-gtr"
+    bin.install "bin/gtr"
+    prefix.install "lib"
+    prefix.install "adapters"
+
+    # Install completions natively
+    bash_completion.install "completions/gtr.bash" => "git-gtr"
+    zsh_completion.install "completions/_git-gtr"
+    fish_completion.install "completions/git-gtr.fish"
+  end
+
+  test do
+    assert_match "gtr version", shell_output("#{bin}/git-gtr version")
+  end
+end
