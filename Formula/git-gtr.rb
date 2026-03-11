65f7f5af80c8a7a68b3d58f84e6da2e9af5f17fd
Tom Elizaga <tom.elizaga@gmail.com>
2026-03-10 20:04:58 -0700

    git-gtr 2.5.0
    Automated update from https://github.com/coderabbitai/git-worktree-runner

--- Formula/git-gtr.rb
+++ Formula/git-gtr.rb
@@ -1,8 +1,8 @@
 class GitGtr < Formula
   desc "Git worktree manager with editor and AI tool integration"
   homepage "https://github.com/coderabbitai/git-worktree-runner"
-  url "https://github.com/coderabbitai/git-worktree-runner/archive/refs/tags/v2.4.0.tar.gz"
-  sha256 "a3e1ee2c6125bd5e82878314238de3e986ca9558c809f724348d17cc9d201a7d"
+  url "https://github.com/coderabbitai/git-worktree-runner/archive/refs/tags/v2.5.0.tar.gz"
+  sha256 "e151ec0bead90ab8a562ca7fdd317a2820ee18cd9527c5441eb3e787df425a87"
   license "Apache-2.0"
 
   depends_on "git"
